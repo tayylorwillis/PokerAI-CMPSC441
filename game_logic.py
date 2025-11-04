c262333da7c3feec89728d504d46108510b53c24
Stewart <madisonlstewart4@gmail.com>
2025-11-04 16:27:18 -0500

    Create game_logic.py

---
+++ game_logic.py
@@ -0,0 +1,60 @@
+""""
+Goals for game_logic:
+generate player hands
+assign hands to players
+detect hand types (ace high)
+get_hand()
+initiate who starts (not our agent for now)
+operate the changing of turns
+take_turn() func
+call()
+fold()
+raise()
+user prompting for user turn
+mode setting for bot v bot or bot v user?
+initialize starting money, will use a var for now,
+take prompt from user later
+ace 14 for now, add is_ace later
+
+NOT: (this is a note to self because I Stew 
+struggle to remember what goes where and will
+start putting everything here otherwise)
+win likelihood for bot logic
+turn logic/decision making for bot
+"""
+
+"""
+A:14
+K:13
+Q:12
+J:11
+10:10...
+"""
+
+#class for card objects
+class Card:
+    def __init__(self):
+        self.suit = 0
+        self.rank = 0
+        #self.is_ace = false
+
+#generate a random card
+def gen_card():
+    
+
+#hand generation func
+def create_hands():
+    i = 2
+    while (i>0):
+
+
+
+#hand type detection func
+def hand_type(hand):
+    if
+
+
+
+
+#hand generation func
+def create_hands():
