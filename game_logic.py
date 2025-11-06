68e548179ee11c26d2c30f76ad9e74db97c4a3e1
Stewart <madisonlstewart4@gmail.com>
2025-11-05 20:17:57 -0500

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -45,9 +45,33 @@ class Card:
 #generate a random card
 def gen_card():
     card = Card()
+    #every card in deck has unique id through 52, decipher which is which accordingly:
     card_num = random.randint(1, 52)
     card.id = card_num
-    card.rank = (card_num % 13) + 2
+    # modulo 13 to get rank, +1 to adjust for ace high
+    card.rank = (card_num % 13) + 1
+    #rank order irrelevant, but divided evenly
+    if ( card_num >= 1 and card_num <= 13):
+        card.suit = "hearts"
+    if ( card_num >= 14 and card_num <= 26):
+        card.suit = "diamonds"
+    if ( card_num >= 27 and card_num <= 39):
+        card.suit = "spades"
+    if ( card_num >= 40 and card_num <= 52):
+        card.suit = "clubs"
+    '''
+    if (card.rank == 14):
+        card.is_ace = true
+    '''
+    return card
+
+#generate a specific card (only used for testing)
+def create_card(card_num):
+    card = Card()
+    card.id = card_num
+    # modulo 13 to get rank, +1 to adjust for ace high
+    card.rank = (card_num % 13) + 1
+    #rank order irrelevant, but divided evenly
     if ( card_num >= 1 and card_num <= 13):
         card.suit = "hearts"
     if ( card_num >= 14 and card_num <= 26):
@@ -65,16 +89,19 @@ def gen_card():
 
 #hand generation func
 def create_hands():
-    deck = list(range(1, 53))
+    #track undealt cards
+    #deck = list(range(1, 53))
     dealt_cards = []
     hand1 = []
     hand2 = []
     i = 0
+    #generate first hand
     while (i < 5):
         c = gen_card()
+        #if new card has already been dealt, generate a new card until it is unique
         while (c.id in dealt_cards):
             c = gen_card()
-        deck.remove(c.id)
+        #deck.remove(c.id)
         dealt_cards.append(c.id)
         hand1.append(c)
         i = i + 1
@@ -84,7 +111,7 @@ def create_hands():
         c = gen_card()
         while (c.id in dealt_cards):
             c = gen_card()
-        deck.remove(c.id)
+        #deck.remove(c.id)
         dealt_cards.append(c.id)
         hand2.append(c)
         i = i + 1
@@ -96,21 +123,140 @@ def print_hand(hand):
            ", ", hand[3].suit, hand[3].rank, ", ", hand[4].suit, hand[4].rank)
 
 
-'''
+
 #hand type detection func
 def hand_type(hand):
-    if
-'''
+    ranks = [hand[0].rank, hand[1].rank, hand[2].rank, hand[3].rank, hand[4].rank]
+    suits = [hand[0].suit, hand[1].suit, hand[2].suit, hand[3].suit, hand[4].suit]
+    max_rank = max(ranks)
+    is_flush = False
+    is_straight = False
+    has_pair = False
+    pair_rank = 0
+    has_three = False
+    three_rank = 0
+    has_two_pair = False
+    second_pair_rank = 0
+    has_four = False
+    four_rank = 0
 
-'''
-c1 = gen_card()
-print(c1.rank)
-print(c1.suit)
+    #check for straight
+    if (max_rank - 1 in ranks and max_rank - 2 in ranks and max_rank - 3 in ranks and 
+        max_rank - 4 in ranks):
+        is_straight = True
+    #check for flush
+    if (suits[0] == suits[1] and suits[0] == suits[2] and suits[0] == suits[3] and
+        suits[0] == suits[4]):
+        is_flush = True
 
-hand1, hand2 = create_hands()
-print_hand(hand1)
-print_hand(hand2)
-'''
+    #check to see if pair is present, tracking what rank the pair is
+    #if pair found, check to see if a three can also be made from that pair
+    #if three found, check for four
+    #if one pair found, look for second
+    #if three found, reset two pair in case three detected as two pair
+    if (ranks[0] == ranks[1] or ranks[0] == ranks[2] or ranks[0] == ranks[3] or ranks[0] == ranks[4]):
+        has_pair = True
+        pair_rank = ranks[0]
+        #check for three
+        if (has_pair and ranks[0] == ranks[1] == ranks[2] or ranks[0] == ranks[1] == ranks[3] or 
+            ranks[0] == ranks[1] == ranks[4] or ranks[0] == ranks[2] == ranks[3] or 
+            ranks[0] == ranks[2] == ranks[4] or ranks[0] == ranks[3] == ranks[5]):
+            has_pair = False
+            has_three = True
+            pair_rank = 0
+            three_rank = ranks[0]
+            #check for four
+            if (has_three and ranks[0] == ranks[1] == ranks[2] == ranks[3] or
+                ranks[0] == ranks[1] == ranks[2] == ranks[4] or
+                ranks[0] == ranks[1] == ranks[3] == ranks[4] or
+                ranks[0] == ranks[2] == ranks[3] == ranks[4]):
+                has_three = False
+                three_rank = 0
+                has_four = True
+                four_rank = ranks[0]
+    if (ranks[1] == ranks[2] or ranks[1] == ranks[3] or ranks[1] == ranks[4]):
+        #if one pair already found, make first pair be second pair and new pair is first pair
+        if (has_pair == True):
+            has_two_pair = True
+            second_pair_rank = pair_rank
+        has_pair = True
+        pair_rank = ranks[1]
+        #check for three
+        if (has_pair and ranks[1] == ranks[2] == ranks[3] or ranks[1] == ranks[2] == ranks[4] or 
+            ranks[1] == ranks[3] == ranks[4]):
+            has_pair = False
+            has_three = True
+            pair_rank = 0
+            three_rank = ranks[1]
+            #reset two pair info in case three has triggered two pair
+            has_two_pair = False
+            second_pair_rank = 0
+            #check for four
+            if (has_three and ranks[1] == ranks[2] == ranks[3] == ranks[4]):
+                has_three = False
+                three_rank = 0
+                has_four = True
+                four_rank = ranks[1]
+    if (ranks[2] == ranks[3] or ranks[2] == ranks[4]):
+        #if one pair already found, make first pair be second pair and new pair is first pair
+        if (has_pair == True):
+            has_two_pair = True
+            second_pair_rank = pair_rank
+        has_pair = True
+        pair_rank = ranks[2]
+        #check for three
+        if (has_pair and ranks[2] == ranks[3] == ranks[4]):
+            has_pair = False
+            has_three = True
+            pair_rank = 0
+            three_rank = ranks[2]
+            #reset two pair info in case three has triggered two pair
+            has_two_pair = False
+            second_pair_rank = 0
+    if (ranks[3] == ranks[4]):
+        #if one pair already found, make first pair be second pair and new pair is first pair
+        if (has_pair == True):
+            has_two_pair = True
+            second_pair_rank = pair_rank
+        has_pair = True
+        pair_rank = ranks[3]
+
+    # return hand type and relevant val in case of tied hand type and 
+    # needing to compare ranks to determine winner
+    if (is_flush and is_straight and max_rank == 14):
+        return "Royal Flush", max_rank
+    
+    if (is_flush and is_straight):
+        return "Straight Flush", max_rank
+    
+    if (has_four):
+        return "Four of a Kind", four_rank
+
+######COME BACK
+    if (has_three and has_pair):
+        return "Full House", three_rank
+
+    if (is_flush):
+        return "Flush", max_rank
+
+    if (is_straight):
+        return "Straight", max_rank
+    
+    if (has_three):
+        return "Three of a Kind", three_rank
+
+    if (has_two_pair):
+        return "Two Pair", max(pair_rank, second_pair_rank)
+
+    if (has_pair):
+        return "One Pair", pair_rank
+
+    return "High Card", max_rank
+
+#four of a kind, 5th spot irrelevant
+#hand = [create_card(1), create_card(14), create_card(27), create_card(40), create_card(2)]
+hand = [create_card(1), create_card(14), create_card(27), create_card(3), create_card(2)]
+print(hand_type(hand))
 
 
 
