f5894da1c83589abd1c402868719440c66cc6891
Stewart <madisonlstewart4@gmail.com>
2025-12-08 14:55:48 -0500

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -1,29 +1,4 @@
 """"
-Goals for game_logic:
-generate player hands
-assign hands to players
-detect hand types (ace high)
-get_hand()
-initiate who starts (not our agent for now)
-operate the changing of turns
-take_turn() func
-call()
-fold()
-raise()
-user prompting for user turn
-mode setting for bot v bot or bot v user?
-initialize starting money, will use a var for now,
-take prompt from user later
-ace 14 for now, add is_ace later
-
-NOT: (this is a note to self because I Stew 
-struggle to remember what goes where and will
-start putting everything here otherwise)
-win likelihood for bot logic
-turn logic/decision making for bot
-"""
-
-"""
 A:14
 K:13
 Q:12
